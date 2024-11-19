a7b2e2a7c6d073c3100538b92a86a94ee36ea16c
greg5320 <art3m43@gmail.com>
2024-11-19 11:38:17 +0300

    feature: add advanced search

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import store from './store';
+import { Provider } from 'react-redux';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(rootElement).render(
   // <React.StrictMode>
+  <Provider store={store}>
     <App />
+  </Provider>
   // </React.StrictMode>
-  
+
 );
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
+  window.addEventListener("load", function () {
     navigator.serviceWorker
       .register("/serviceWorker.js")
       .then(() => console.log("service worker registered"))
--- src/pages/MapList.tsx
+++ src/pages/MapList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import { Button, FormControl, Container, Row, Col, Image } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/MapList.css';
@@ -7,21 +8,26 @@ import { Map } from '../modules/mapApi';
 import { mockMaps } from '../modules/mockData';
 import Header from '../components/Header';
 import { BreadCrumbs } from '../components/BreadCrumbs';
+import { RootState } from '../store';
+import { setSearchTerm } from '../store/searchSlice';
+
 const MapList: React.FC = () => {
   const [maps, setMaps] = useState<Map[]>([]);
-  const [searchTerm, setSearchTerm] = useState<string>('');
   const [error, setError] = useState<string | null>(null);
 
   const defaultImageUrl = 'http://127.0.0.1:9000/mybucket/map_not_found.png';
+  const navigate = useNavigate();
+
+  const searchTerm = useSelector((state: RootState) => state.search.searchTerm);
+  const dispatch = useDispatch();
   const filterMaps = (title: string) => {
     if (title) {
-      return mockMaps.filter(map =>
+      return mockMaps.filter((map) =>
         map.title.toLowerCase().includes(title.toLowerCase())
       );
     }
     return mockMaps;
   };
-
   const fetchMaps = async (title: string = '') => {
     try {
       const response = await fetch(`/maps/?title=${title}`);
@@ -45,29 +51,35 @@ const MapList: React.FC = () => {
   };
 
   useEffect(() => {
-    fetchMaps();
-  }, []);
+    fetchMaps(searchTerm); 
+  }, [searchTerm]);
 
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const query = event.target.value;
-    setSearchTerm(query);
-    fetchMaps(query);
+ 
+  const handleSearch = (event: React.FormEvent) => {
+    event.preventDefault();
+    navigate(`/maps?title=${encodeURIComponent(searchTerm)}`); 
+    fetchMaps(searchTerm);
+  };
+
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearchTerm(event.target.value));
   };
 
   return (
     <>
       <Header />
-      <BreadCrumbs crumbs={[{ label: "Карты", path: "/maps" }]} /> 
-      <Container >
+      <BreadCrumbs crumbs={[{ label: 'Карты', path: '/maps' }]} />
+      <Container>
         <h3>Список карт</h3>
-        <form className="find-button" onSubmit={(e) => e.preventDefault()}>
+        <form className="find-button" onSubmit={handleSearch}>
           <Row className="justify-content-center align-items-center">
             <Col xs={12} sm={8} md={6} lg={4} className="search-field">
               <FormControl
                 type="text"
                 placeholder="Поиск по названию"
-                value={searchTerm}
-                onChange={handleSearch}
+                value={searchTerm} 
+                onChange={handleInputChange}
                 className="mb-2"
               />
             </Col>
@@ -81,7 +93,14 @@ const MapList: React.FC = () => {
         <Row className="map-container mt-4">
           {maps.length > 0 ? (
             maps.map((map) => (
-              <Col xs={12} sm={6} md={4} lg={3} key={map.id} className="map-item mb-4">
+              <Col
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                key={map.id}
+                className="map-item mb-4"
+              >
                 <Link to={`/maps/${map.id}`}>
                   <Image
                     src={map.image_url ? map.image_url : defaultImageUrl}

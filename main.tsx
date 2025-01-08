9b06b51a66759a93bcb891819a37dd3ff9a261e1
greg5320 <art3m43@gmail.com>
2025-01-08 16:43:08 +0300

    bug fix

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import store from './store';
+import App from './src/App';
+import store from './src/store';
 import { Provider } from 'react-redux';
 
 const rootElement = document.getElementById('root') as HTMLElement;

80343ce0e66c64500e66b8e1c514798988023b33
greg5320 <art3m43@gmail.com>
2025-01-08 18:00:04 +0300

    bug fix

--- src/modules/axios.ts
+++ src/modules/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',  
+  baseURL: 'http://62.113.42.126:8000',  
   headers: {
     'Content-Type': 'application/json',
     'X-CSRFToken': Cookies.get('csrftoken'),
